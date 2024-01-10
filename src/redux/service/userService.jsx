df81725eb5f4594eec07344b36b518a2f4635bca
HuuSyC0323H1 <nguyenhuusy.mtlp@gmail.com>
2024-01-10 10:40:37 +0700

    reload fix login register

--- src/redux/service/userService.jsx
+++ src/redux/service/userService.jsx
@@ -1,7 +1,5 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
 import {UrlUser} from "../../context/connect.jsx";
-import {toast} from "react-toastify";
-import {useNavigate} from "react-router-dom";
 
 export const login = createAsyncThunk(
     "user/login",
@@ -12,6 +10,5 @@ export const login = createAsyncThunk(
 )
 
 export const register = (data) => {
-    toast.success("Vui lòng kiểm khoản email và kích hoạt để bắt đầu sử dụng")
-    return UrlUser().post("register",data);
+    return UrlUser().post("register", data);
 }
